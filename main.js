081c3162d9bcfa19b2694e4c37fdfb132d0c8576
ayush <ayushkumarsingh97@gmail.com>
2016-06-03 19:41:21 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Ayush Singh</title>
+
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
+		<script src="js/modernizr.custom.js"></script>
+
+		<link href='http://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+
+
+
+
+  </body>
+  </html>
