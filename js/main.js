2a2ea755018df22596fa69e69a2a90c84bc94dae
ayush <ayushkumarsingh97@gmail.com>
2016-06-21 08:45:20 +0530

    parallax adde to div

--- js/main.js
+++ js/main.js
@@ -113,7 +113,8 @@ particlesJS("particles-js", {
 
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
-
+var scene1 = document.getElementById('scene1');
+var parallax = new Parallax(scene1);
 
 //audi vis
 //
